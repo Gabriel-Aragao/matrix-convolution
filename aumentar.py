7b12d708400d89eca72d4601b66ec9f883a62e3f
Gabriel-Aragao <batista.gabriel.aragao@gmail.com>
2025-05-07 15:24:52 -0300

    Final commit

--- aumentar.py
+++ aumentar.py
@@ -17,6 +17,7 @@ def aumentar_com_media(imagem):
     for i in range(len(imagem)*2):
         nova_imagem.append([])
         for j in range(len(imagem[0])*2):
+            
             if(j%2!=0 and j//2 < len(imagem[0])-1):
                 azul = (int(imagem[i//2][j//2][0]) + 
                          int(imagem[i//2][j//2+1][0])) // 2
@@ -25,6 +26,7 @@ def aumentar_com_media(imagem):
                 vermelho = (int(imagem[i//2][j//2][2]) + 
                             int(imagem[i//2][j//2+1][2])) // 2
                 nova_imagem[i].append([azul,verde,vermelho])
+            
             elif(i%2!=0 and i//2 < len(imagem)-1):
                 azul = (int(imagem[i//2][j//2][0]) + 
                         int(imagem[i//2+1][j//2][0])) // 2
@@ -33,6 +35,7 @@ def aumentar_com_media(imagem):
                 vermelho = (int(imagem[i//2][j//2][2]) + 
                             int(imagem[i//2+1][j//2][2])) // 2
                 nova_imagem[i].append([azul,verde,vermelho])
+            
             elif(j%2!=0 and i%2!=0 and j//2 < len(imagem[0])-1 and i//2 < len(imagem_lista)-1):
                 azul = (int(imagem[i//2+1][j//2][0]) +
                         int(imagem[i//2][j//2+1][0])) // 2
@@ -41,6 +44,7 @@ def aumentar_com_media(imagem):
                 vermelho = (int(imagem[i//2+1][j//2][2]) +
                             int(imagem[i//2][j//2+1][2])) // 2
                 nova_imagem[i].append([azul,verde,vermelho])
+            
             else:
                 nova_imagem[i].append(imagem[i//2][j//2])
 
